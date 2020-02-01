f13fd8b5d3e53befdd12bd04f4715c934cc7a850
Patience Condell <condellp@gmail.com>
2020-02-01 11:48:59 -0500

    learning

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -1,27 +1,16 @@
-// ===============================================================================
-// DEPENDENCIES
-// We need to include the path package to get the correct file path for our html
-// ===============================================================================
-var path = require("path");
-
-// ===============================================================================
-// ROUTING
-// ===============================================================================
+// Requirements
+var path = require('path');
+// Routing via express
 
 module.exports = function(app) {
 
-  // HTML GET Requests
-  // Below code handles when users "visit" a page.
-  // In each of the below cases the user is shown an HTML page of content
-  // ---------------------------------------------------------------------------
-
-  app.get("/survey", function(req, res) {
-    res.sendFile(path.join(__dirname, "/../public/survey.html"));
+  app.get('', function(req, res) {
+    res.sendFile(path.join(__dirname, './../'));
   });
 
   // If no matching route is found default to home
   app.get("*", function(req, res) {
-    res.sendFile(path.join(__dirname, "/../public/home.html"));
+    res.sendFile(path.join(__dirname, "../public/home.html"));
   });
 
 };
--- server.js
+++ server.js
@@ -1,20 +1,28 @@
-// Dependencies
-var express = require("express");
-var bodyParser = require("body-parser");
-
+// Add the requirements for server
+var bodyParser = require('body-parser');
+var express = require('express');
+var path = require('path');
+// Create express app/port info
 var app = express();
+var PORT = process.env.PORT | 3000;
 
-var PORT = process.env.PORT || 8080;
-
+// Parse application and JSON data
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.text());
+app.use(bodyParser.json({type: 'application/vnd.api+json'}));
 
+// Create routes for express
+// require('./app/routing/apiRoutes')(app);
+// require('./app/routing/htmlRoutes')(app)
+const htmlRoutes = require('./app/routing/htmlRoutes');
+const apiRoutes = require('./app/routing/apiRoutes');
 
+app.use(express.static(path.join(__dirname, "./public/home.html")))
+app.use('/', htmlRoutes)
 
-// API and HTML routes
-require("./app/routing/apiRoutes")(app);
-require("./app/routing/htmlRoutes")(app);
+// Creating a listening function
 
 app.listen(PORT, function() {
-    console.log("App listening on PORT: " + PORT);
-});
+    console.log("App Listening on PORT " + PORT)
+})
